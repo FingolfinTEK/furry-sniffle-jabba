ae5ddcbffecae601e171d834dc964e77ab8206d1
Milos Milivojevic <mmilivoj@amazon.com>
2018-04-16 11:16:50 +0200

    Zetas handler

---
+++ src/main/kotlin/com/fingolfintek/bot/handler/ZetasHandler.kt
@@ -0,0 +1,44 @@
+package com.fingolfintek.bot.handler
+
+import com.fingolfintek.swgohgg.guild.GuildChannelRepository
+import com.fingolfintek.swgohgg.unit.UnitRepository
+import io.vavr.control.Try
+import net.dv8tion.jda.core.entities.Message
+import org.springframework.stereotype.Component
+
+@Component
+open class ZetasHandler(
+    private val unitRepository: UnitRepository,
+    private val guildChannelRepository: GuildChannelRepository
+) : MessageHandler {
+
+  private val messageRegex = Regex(
+      "!zetas\\s+(.+)",
+      setOf(RegexOption.IGNORE_CASE, RegexOption.DOT_MATCHES_ALL)
+  )
+
+  override fun isApplicableTo(message: Message): Boolean =
+      message.content.trim().matches(messageRegex)
+
+  override fun processMessage(message: Message) {
+    Try.ofSupplier { messageRegex.matchEntire(message.content)!! }
+        .andThenTry { match ->
+          val playerName = match.groupValues[1].trim()
+
+          guildChannelRepository
+              .getRosterForChannel(message.channel.id)
+              .filter { it.name == playerName }
+              .map { it.units.filter { it.zetas.isNotEmpty() } }
+              .forEach { zetadUnits ->
+                zetadUnits
+                    .sortBy { it.unit.name }
+                    .map { "${it.unit.name}\n\t${it.zetas.joinToString("\n\t")}" }
+                    .joinToString("\n\n")
+                    .let { message.respondWithEmbed("$playerName zetas", it) }
+              }
+        }
+        .onFailure {
+          sendErrorMessageFor(it, message)
+        }
+  }
+}
