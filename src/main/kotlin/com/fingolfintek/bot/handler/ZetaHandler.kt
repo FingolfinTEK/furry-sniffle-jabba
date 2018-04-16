dd91dead88948917a759dbab26b5502ba8af31b5
Milos Milivojevic <mmilivoj@amazon.com>
2018-04-16 11:05:10 +0200

    Indexed output

--- src/main/kotlin/com/fingolfintek/bot/handler/ZetaHandler.kt
+++ src/main/kotlin/com/fingolfintek/bot/handler/ZetaHandler.kt
@@ -7,7 +7,7 @@ import net.dv8tion.jda.core.entities.Message
 import org.springframework.stereotype.Component
 
 @Component
-open class ZetasHandler(
+open class ZetaHandler(
     private val unitRepository: UnitRepository,
     private val guildChannelRepository: GuildChannelRepository
 ) : MessageHandler {
@@ -33,10 +33,11 @@ open class ZetasHandler(
               }
               .getOrElse { guildRoster.filter { it.units.any { it.zetas.contains(unitOrZeta) } } }
               .sortedBy { it.name }
+              .withIndex()
               .joinToString(
                   prefix = "__**$unitOrZeta**__\n\n",
                   separator = "\n",
-                  transform = { it.name })
+                  transform = { "${it.index} ${it.value.name}" })
               .let { message.respondWithEmbed("Players with $unitOrZeta zeta", it) }
         }
         .onFailure {
